253df10857a38138473881e6affd41e72198847a
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2022-01-28 19:02:11 -0300

    hotfix

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = app => {
     fs
-        .readSync(__dirname)
-        .filter(file => ((file.indexOf('.')) !== 0 && (file !== "indexe.js")))
+        .readdirSync(__dirname)
+        .filter(file => ((file.indexOf('.')) !== 0 && (file !== "index.js")))
         .forEach(file => require(path.resolve(__dirname, file))(app))
 }
